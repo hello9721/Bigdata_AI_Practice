01f421831357b1816be611eb16d73018b5e7d3b8
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-15 11:01:31 +0900

    Update Ex20220915_1.R

--- Day 5/Ex20220915_1.R
+++ Day 5/Ex20220915_1.R
@@ -21,3 +21,48 @@ colnames(wide) = c('Customer_ID', 'Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', '
 write.csv(wide, "wide_.csv", row.names = F)     # 외부파일로 내보내기
 
 wide
+
+
+# melt(데이터, 기준행)
+# 넓은 형식 -> 긴 형식
+
+long <- melt(wide, id = "Customer_ID")
+long
+
+colnames(long) <- c('Customer_ID', 'Date', 'Buy')
+long                                            # 열 이름 지정
+
+
+# 실습
+
+data("smiths")
+smiths
+
+long <- melt(id = 1:2, smiths)                  # 1번 열 기준
+long
+
+wide <- dcast(long, subject+time ~ ...)         # a+b -> a와 b 기준으로
+wide                                            # ... -> 나머지 모두
+
+
+# acast(데이터, 행 기준 ~ 열 기준 ~ 면 기준)
+# array( = 3차원 구조 배열)로 변경
+
+data('airquality')
+airquality
+
+str(airquality)
+
+names(airquality) <- toupper(names(airquality)) # toupper(name) -> 열 이름을 대문자로 변경
+airquality                                      # names(data) -> 데이터의 열이름 가져오기
+
+air_melt <- melt(airquality, id = c('MONTH', 'DAY'), na.rm = T)
+                                                # c로 묶어서 여러 기준을 잡을 수 있다.
+                                                # na.rm = T -> na를 제거
+air_melt
+
+names(air_melt) <- tolower(names(air_melt))     # tolower(name) -> 열 이름을 소문자로 변경
+air_melt
+
+air_3d <- acast(air_melt, day ~ month ~ variable)
+air_3d
