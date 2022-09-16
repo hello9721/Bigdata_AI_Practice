f9ba6b3f87c3c9b624ebf52126154ef8f8113d16
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-16 12:57:20 +0900

    Update Ex20220916_1.R

--- Day 6/Ex20220916_1.R
+++ Day 6/Ex20220916_1.R
@@ -104,9 +104,8 @@ dim(testing)
 # 동일한 데이터셋을 N등분하여 N-1개의 학습데이터로 하고
 # 나머지 1을 검정데이터로 이용
 
-# K겹 교차 검정 데이터셋 알고리즘 사용
-# EX) N = 3 => N1 = D1, D2, D3 | N2 = D1, D2, D3 | N3 = D1, D2, D3
-# EX) K = 3 => 검정:학습1/학습2 => N1 -> D1:D2/D3 | N2 -> D2:D1/D3 | N3 -> D3:D1/D2
+# K겹 교차 검정 데이터셋 알고리즘 사용 ( K fold )
+# EX) K = 3 => 검정:학습1/학습2 => 1겹 -> D1:D2/D3 | 2겹 -> D2:D1/D3 | 3겹 -> D3:D1/D2
 
 name <- c('a', 'b', 'c', 'd', 'e', 'f')
 score <- c(90, 85, 99, 75, 65, 88)
@@ -117,3 +116,74 @@ df <- data.frame(Name = name, Score = score)
 install.packages("cvTools")
 library(cvTools)
                                           # 교차 검정을 위한 패키지 설치 및 로드
+cross <- cvFolds(n = 6, K = 3, R = 1, type = "random")
+                                          # cvFolds(n = 요소 개수, K = K겹, R = 반복횟수, type = "추출 방법" )
+                                          # K겹 교차 검정 데이터 셋 생성
+str(cross)
+
+cross$subsets[cross$which == 1, 1]        # K = 1인 경우 -> 2, 6 = test : train = 5, 4, 3, 1  
+cross$subsets[cross$which == 2, 1]        # K = 2인 경우 -> 1, 5 = test : train = 6, 4, 3, 2
+cross$subsets[cross$which == 3, 1]        # K = 3인 경우 -> 3, 4 = test : train = 6, 5, 2, 1
+
+r = 1                                     # 반복 횟수
+K = 1:3                                   # K겹 교차 검정
+
+for(i in K){                              # K회 반복
+  data_ind <- cross$subsets[cross$which == i, r]
+                                          # data_ind에 which가 i인 r열 데이터를 임시로 담는다.
+  cat('K = ', i, '검정데이터 \n')
+  print(df[data_ind, ])
+                                          # data_ind를 인덱스 삼아 df에서 검정데이터를 추출한다.
+  cat('K = ', i, '훈련데이터 \n')
+  print(df[-data_ind, ])                  # 검정데이터의 인덱스를 제외한 나머지 데이터를 훈련데이터로 추출한다.
+}
+
+
+
+# 3 실습 예제
+
+# airquality 데이터 셋 처리
+
+# 기온이 가장 높은 날 -> 월/일 추출
+# 6월달 발생한 가장 강한 바람의 세기 추출
+# 7월달의 평균 기온 추출
+# NA를 제외한 5월의 평균 오존 농도 추출
+# 오존 농도가 100을 넘는 날은 며칠인지 추출
+
+data("airquality")                        # 데이터셋 로드
+
+str(airquality)
+
+# 기온이 가장 높은 날 -> 월/일 추출
+
+top_temp <- data.frame(Month = 0, Day = 0)
+                                          # 날짜 값을 담아줄 데이터프레임 생성
+top_temp$Month <- airquality$Month[ airquality$Temp == max(airquality$Temp, na.rm = T)]
+top_temp$Day <- airquality$Day[ airquality$Temp == max(airquality$Temp, na.rm = T)]
+                                          # max를 이용하여 temp 열이 max 값인 month와 day를 추출하여 담음
+cat(top_temp$Month,"/",top_temp$Day)
+                                          # 출력
+
+# 6월달 발생한 가장 강한 바람의 세기 추출
+
+June_strong <- max(airquality$Wind[airquality$Month == 6], na.rm = T)
+                                          # max를 이용하여 month가 6인 행들 중 wind 열 값이 큰 것을 가져옴
+cat(June_strong)
+
+# 7월달의 평균 기온 추출
+
+July_avg_temp <- mean(airquality$Temp[airquality$Month == 7])
+                                          # mean을 이용하여 month가 7인 행들의 평균 값
+cat(July_avg_temp)
+
+# NA를 제외한 5월의 평균 오존 농도 추출
+
+May_avg_ozone <- mean(airquality$Ozone[airquality$Month == 5], na.rm = T)
+                                          # mean의 na.rm 속성을 이용하여 na를 모두 제거하고 난 5월의 오존 평균 값
+cat(May_avg_ozone)
+
+# 오존 농도가 100을 넘는 날은 며칠인지 추출
+
+ozone_over100 <- length(airquality$Ozone[airquality$Ozone >= 100])
+                                          # length를 이용하여 ozone의 값이 100을 넘는 값들의 갯수를 카운트
+ozone_over100
