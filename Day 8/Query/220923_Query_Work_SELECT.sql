83a50f8b0949faad030699a24f57f994d61aacb0
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-23 17:17:17 +0900

    Update 220923_Query_Work_SELECT.sql

--- Day 8/Query/220923_Query_Work_SELECT.sql
+++ Day 8/Query/220923_Query_Work_SELECT.sql
@@ -14,9 +14,18 @@ SELECT u.uName, p.pName, s.sCount
 	WHERE u.uID = s.uID AND  p.pCode = s.pCode AND u.uID = 'A2';
 
 
-/* 새로운 열 생성 및 열 연산 및 열 이름 변경 */
+/* 새로운 열 생성 | 열 연산 | 열 이름 변경 */
 
 SELECT u.uID ID, u.uName AS Name, u.uAge Age, p.pName Product, p.pCode AS Code, s.sCount Count, p.pPrice * s.sCount AS Total_Price
 	FROM shopuser u, shopsale s, shopproduct p
 	WHERE u.uID = s.uID AND s.pCode = p.pCode;
 	
+/* Sub Query == Join Query */
+/* 조회한 결과를 다른 조회 구문의 입력으로 사용 = 부분 조회 */
+
+/* 세탁기를 구매한 고객의 이름 */
+/* 구매 내역은 shopsale에 상품명은 shopproduct에 고객명은 shopuser에 있기에 까다롭다. */
+
+SELECT uName FROM shopuser WHERE uID = 
+	(SELECT	uID FROM shopsale WHERE pCode = 
+		(SELECT pCode FROM shopproduct WHERE pName = '세탁기'));
