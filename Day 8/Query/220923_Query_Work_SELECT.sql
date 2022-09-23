ae6e2023b37ade7e1446a8d87da8d6c70eaf638b
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-23 17:04:54 +0900

    Update 220923_Query_Work_SELECT.sql

--- Day 8/Query/220923_Query_Work_SELECT.sql
+++ Day 8/Query/220923_Query_Work_SELECT.sql
@@ -1,6 +1,6 @@
 USE WORK;
 
-/* Join Query  => ( 3 TABLE -> 1 TABLE ) */
+/* Join Query  => ( Many TABLE -> 1 TABLE ) */
 
 SELECT u.uID, u.uName, u.uAge, p.pName, p.pCode, s.sCount, p.pPrice
 	FROM shopuser u, shopsale s, shopproduct p
@@ -13,3 +13,10 @@ SELECT u.uName, p.pName, s.sCount
 	FROM shopuser u, shopsale s, shopproduct p 
 	WHERE u.uID = s.uID AND  p.pCode = s.pCode AND u.uID = 'A2';
 
+
+/* 새로운 열 생성 및 열 연산 및 열 이름 변경 */
+
+SELECT u.uID ID, u.uName AS Name, u.uAge Age, p.pName Product, p.pCode AS Code, s.sCount Count, p.pPrice * s.sCount AS Total_Price
+	FROM shopuser u, shopsale s, shopproduct p
+	WHERE u.uID = s.uID AND s.pCode = p.pCode;
+	
