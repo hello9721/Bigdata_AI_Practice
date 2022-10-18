15e52bef5b7e2b7c3b16b48b785b588b874a95c7
Hana <95046369+hello9721@users.noreply.github.com>
2022-10-18 16:49:11 +0900

    Add files via upload

---
+++ PACKAGE.R
@@ -0,0 +1,6 @@
+install.packages("RSADBE")                        # 샘플 데이터 셋 패키지
+install.packages("UsingR")
+
+install.packages("stringr")                       # 문자열 연산을 위한 패키지
+install.packages("readxl")                        # 엑셀파일 (.xlsx)을 읽기 위한 패키지
+install.packages("writexl")                       # 엑셀파일 (.xlsx)로 저장을 위한 패키지
