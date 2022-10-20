af9d308d5eb6829657ca52cdba21ffb464d3f13e
hello9721 <hanya97@kakao.com>
2022-10-20 14:17:40 +0900

    package

--- PACKAGE.R
+++ PACKAGE.R
@@ -1,6 +1,34 @@
 install.packages("RSADBE")                        # 샘플 데이터 셋 패키지
 install.packages("UsingR")
+install.packages("hflights")
+install.packages("mlmRev")
+install.packages("datasets")                    
 
 install.packages("stringr")                       # 문자열 연산을 위한 패키지
+install.packages("dplyr")                         # 파이프연산자, tbl_df, filter, arrange, rename,
+#                                                 # select, mutate, summarise, group_by, join, bind,
+install.packages("reshape2")                      # dcast, melt, acast
+install.packages("cvTools")                       # 교차 검정을 위한 패키지
+install.packages("RJDBC")                         # mariaDB 연결을 위한 패키지
+install.packages("DBI")
 install.packages("readxl")                        # 엑셀파일 (.xlsx)을 읽기 위한 패키지
 install.packages("writexl")                       # 엑셀파일 (.xlsx)로 저장을 위한 패키지
+install.packages("httr")                          # 원격 서버의 URL 요청을 위한 패키지
+install.packages("XML")                           # HTML 문서 파싱을 위한 패키지
+install.packages("moments")                       # 왜도, 첨도 를 위한 패키지
+install.packages("gmodels")                       # 교차분할표, 카이제곱을 위한 패키지
+install.packages("prettyR")                       # freq
+install.packages("memisc")                        # spss 데이터를 가져오기 위한 패키지
+install.packages("car")                           # 분산팽창요인 확인을 위한 패키지
+install.packages("party")                         # 의사결정트리를 위한 패키지
+
+
+install.packages("scatterplot3d")                 # 3차원 산점도 시각화
+install.packages("lattice")                       # histogram, densityplot, barchart, dotplot,
+#                                                 # xyplot, equal.count, coplot, cloud
+install.packages("ggplot2")                       # qplot, geom, ggplot, theme, ggsave
+install.packages("ggmap")                         # get_stamenmap 등 지도를 이용한 시각화
+install.packages("wordcloud")                     # 단어 구름 시각화
+install.packages("corrgram")                      # 상관계수 시각화
+install.packages("PerformanceAnaltics")           # 밀도곡선, 상관성, 유의확률 추가 시각화
+install.packages("ROCR")                          # ROC Curve의 시각화
